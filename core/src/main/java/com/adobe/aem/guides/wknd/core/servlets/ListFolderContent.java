132141d2d210c786dc8858941c88f117a5b6727b
vkatoch <vkatoch07@gmail.com>
2023-01-24 12:13:46 +0530

    new

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/servlets/ListFolderContent.java
@@ -0,0 +1,54 @@
+package com.adobe.aem.guides.wknd.core.servlets;
+
+import static com.day.cq.commons.jcr.JcrConstants.JCR_CONTENT;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.Servlet;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+@Component(service = {Servlet.class}, 
+			property = {"sling.servlet.methods=get",
+						"sling.servlet.paths=/bin/listfoldercontents"
+})
+public class ListFolderContent extends SlingSafeMethodsServlet {
+  private static final long serialVersionUID = 1L;
+  private static final Logger log = LoggerFactory.getLogger(ListFolderContent.class);
+  protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
+    Resource resource = request.getResourceResolver().getResource(request.getParameter("damFolder"));
+    List < JsonObject > results = new ArrayList < > ();
+    resource.getChildren().forEach(child -> {
+      if (!JCR_CONTENT.equals(child.getName())) {
+        JsonObject asset = new JsonObject();
+        log.debug("##The child name is " + child.getName());
+        asset.addProperty("assetname", child.getName());
+        asset.addProperty("assetpath", child.getPath());
+        results.add(asset);
+
+      }
+    });
+    PrintWriter out = null;
+    try {
+      out = response.getWriter();
+    } catch (IOException e) {
+
+      log.debug(e.getMessage());
+    }
+    response.setContentType("application/json");
+    response.setCharacterEncoding("UTF-8");
+    Gson gson = new Gson();
+    out.print(gson.toJson(results));
+    out.flush();
+  }
+
+}
+
